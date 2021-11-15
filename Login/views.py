eeb7e5ba0add6ea2198708b9fc4a9df000af75e5
Narges <asadi.moh16@gmail.com>
2021-11-15 22:34:17 +0330

    Edit

--- Login/views.py
+++ Login/views.py
@@ -36,7 +36,21 @@ def createsignup (request):
 
 def profile(request, name):
     profile = models.User_signup.objects.get(name=name)
-    return render(request, 'Login/profile.html', {'profile': profile})
+    # if request.method == 'POST':
+    #     return render(request, 'Login/edit.html', {'edit: '})
+    # else:
+    if request.method== 'POST':
+        formedit= forms.CreateSignup(request.POST)
+        if formedit.is_valid():
+            profile.delete()
+            formedit.save()
+            # instanc = formedit.save(commit=False)
+    #         # INja shart bezaram
+    #         formedit.save()
+            return redirect('Login:list')
+    else:
+        formedit = forms.CreateSignup()
+    return render(request, 'Login/profile.html', {'profile': profile , 'form': formedit})
 
 @login_required(login_url="/accounts/login2")
 def create_article(request):
